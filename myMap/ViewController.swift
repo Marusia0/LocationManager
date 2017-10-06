ce55f5ec5b91e70a699969bf041078ccbc06be2b
Marusia0 <lunapelota@hotmail.com>
2017-10-06 11:53:39 -0700

    ///Comit
    Friday 6

--- myMap/ViewController.swift
+++ myMap/ViewController.swift
@@ -18,21 +18,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
-        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(1.0, 1.0)
         let userLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region: MKCoordinateRegion = MKCoordinateRegionMake(userLocation, span)
         
         myMap.setRegion(region, animated: true)
+
+        print(location.altitude)
+        print(location.speed)
+        
         self.myMap.showsUserLocation = true
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let mark : MKPointAnnotation = MKPointAnnotation()
+        //mark.coordinate = location
+        mark.title = "Info"
+        mark.subtitle = "click here"
+        myMap.addAnnotation(mark)
     }
 }
 
